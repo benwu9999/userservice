2a0600346d0019d2a24560adbec5a44d17a73026
youlepan <youlepan@gmail.com>
2017-04-16 00:33:53 +0000

    fixed a type for HttpResponse

--- UserService/userapp/views.py
+++ UserService/userapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpRespose("Hello world! This is our User App.")
+	return HttpResponse("Hello world! This is our User App.")
 
 from rest_framework import generics 
 from userapp.models import User
