a54d9b9fd16f557e1ad08c97716084b77722e367
Youle Pan <youlepan@Youles-MBP.fios-router.home>
2017-04-16 21:47:59 -0400

    remove jobpost app as installed application

--- UserService/mysite/settings.py
+++ UserService/mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'jobpostapp.apps.JobPostAppConfig',
     'userapp.apps.UserAppConfig',
 ]
 
