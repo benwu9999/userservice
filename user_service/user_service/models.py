991333d1f8a27db5a4ddde81a24e1c18656b43fa
youlepan <youlepan@gmail.com>
2017-05-02 22:20:01 -0400

    enabled PermissionsMixin

--- user_service/user_service/models.py
+++ user_service/user_service/models.py
@@ -2,13 +2,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.mail import send_mail
 from django.contrib.auth.base_user import AbstractBaseUser
-# from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import validate_email, RegexValidator
 from .managers import UserManager
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     """
     data model for User
     """
