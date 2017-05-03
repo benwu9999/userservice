144d3096772f61daa6dbbf77d7d8ac9e0db0e35e
youlepan <youlepan@gmail.com>
2017-05-03 16:16:09 -0400

    setting up the urlpatterns for User views

--- user_service/user_service/urls.py
+++ user_service/user_service/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 urlpatterns=[
-	url(r'^$', views.index, name='index'),
-	url(r'^user/$', views.UserList.as_view()),
+    url(r'^$', views.index, name='index'),
+    url(r'^user/$', views.UserList.as_view()),
+    url(r'^user/(?P<userId>[0-9a-f-]+)$', views.UserDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
