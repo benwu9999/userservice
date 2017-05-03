38b024a3f0a8a789828dcfcb4857ae7f54d1b73f
youlepan <youlepan@gmail.com>
2017-05-03 16:15:01 -0400

    Implement Generic View for User Model

--- user_service/user_service/views.py
+++ user_service/user_service/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from rest_framework import generics 
+from .models import User
+from .serializers import UserSerializer
+
 
 def index(request):
-	return HttpResponse("Hello world! This is our User App.")
+    return HttpResponse("Hello world! This is our User App.")
 
-from rest_framework import generics 
-from userapp.models import User
-from userapp.serializers import UserSerializer
 
 class UserList(generics.ListCreateAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    # override the default lookup field "PK" with the lookup field for this model
+    lookup_field = 'userId'
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
