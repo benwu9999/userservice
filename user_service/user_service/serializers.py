42c993189333a1199f1b357e030f0aef994d46f6
youlepan <youlepan@gmail.com>
2017-05-03 20:29:18 -0400

    added condition to support User object and Dict instances

--- user_service/user_service/serializers.py
+++ user_service/user_service/serializers.py
@@ -6,14 +6,15 @@ class UserSerializer(serializers.ModelSerializer):
     """
     Serializer to convert User object data to primitive Python datatypes
     """
-    profileIds = serializers.ListField(child=serializers.UUIDField(format='hex_verbose'), min_length=None,
-                                       max_length=None)
     locationIds = serializers.ListField(child=serializers.UUIDField(format='hex_verbose'), min_length=None,
                                         max_length=None)
+    profileIds = serializers.ListField(child=serializers.UUIDField(format='hex_verbose'), min_length=None,
+                                       max_length=None)
     applicationIds = serializers.ListField(child=serializers.UUIDField(format='hex_verbose'), min_length=None,
                                            max_length=None)
     roles = serializers.ListField(child=serializers.UUIDField(format='hex_verbose'), min_length=None,
                                   max_length=None)
+
 
     class Meta:
         model = User
@@ -27,8 +28,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     # Comma Separated String to List
     def string_to_list(self, data):
-        if (data is not None) and (not isinstance(data, list)):
-            data = [i for i in data.split(',')]
+        if not isinstance(data, list):
+            if data:
+                data = [i for i in data.split(',')]
+            else:
+                data = []
         return data
 
     # List to Comma Separated String
@@ -41,16 +45,22 @@ class UserSerializer(serializers.ModelSerializer):
         return data
 
     def to_representation(self, instance):
-        instance.profileIds = self.string_to_list(instance.profileIds)
-        instance.locationIds = self.string_to_list(instance.locationIds)
-        instance.applicationIds = self.string_to_list(instance.applicationIds)
-        instance.roles = self.string_to_list(instance.roles)
+        if isinstance(instance, dict):
+            instance['locationIds'] = self.string_to_list(instance['locationIds'])
+            instance['profileIds'] = self.string_to_list(instance['profileIds'])
+            instance['applicationIds'] = self.string_to_list(instance['applicationIds'])
+            instance['roles'] = self.string_to_list(instance['roles'])
+        else:
+            instance.locationIds = self.string_to_list(instance.locationIds)
+            instance.profileIds = self.string_to_list(instance.profileIds)
+            instance.applicationIds = self.string_to_list(instance.applicationIds)
+            instance.roles = self.string_to_list(instance.roles)
         return super(UserSerializer, self).to_representation(instance)
 
     def to_internal_value(self, data):
         ret = super(UserSerializer, self).to_internal_value(data)
-        ret['profileIds'] = self.list_to_string(ret['profileIds'])
         ret['locationIds'] = self.list_to_string(ret['locationIds'])
+        ret['profileIds'] = self.list_to_string(ret['profileIds'])
         ret['applicationIds'] = self.list_to_string(ret['applicationIds'])
         ret['roles'] = self.list_to_string(ret['roles'])
         return ret
