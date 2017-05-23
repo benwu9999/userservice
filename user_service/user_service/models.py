6024f9e4332a3e6a99169ace9e8b2797561b5743
youlepan <youlepan@gmail.com>
2017-05-22 21:16:24 -0400

    changed db_name to db_table

--- user_service/user_service/models.py
+++ user_service/user_service/models.py
@@ -36,7 +36,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['email']
 
     class Meta:
-        db_name = 'user'
+        db_table = 'user'
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
